96452d1efc56352ce601977892e4bbad0c4e6400
wyn <k022298@gmail.com>
2022-11-29 21:21:58 +0000

    Modify the folder of coordinator

--- TechTest/Application/MainCoordinator.swift
+++ TechTest/Application/MainCoordinator.swift
@@ -12,35 +12,10 @@ enum AppFlow {
     case Favorites
 }
 
-protocol FlowCoordinator: AnyObject {
-    var parentCoordinator: MainBaseCoordinator? { get set }
-}
-
-protocol Coordinator: FlowCoordinator {
-    var rootViewController: UIViewController { get set }
-    func start() -> UIViewController
-    @discardableResult func resetToRoot() -> Self
-}
-
-extension Coordinator {
-    var navigationRootViewController: UINavigationController? {
-        get {
-            (rootViewController as? UINavigationController)
-        }
-    }
-    
-    func resetToRoot() -> Self {
-        navigationRootViewController?.popToRootViewController(animated: false)
-        return self
-    }
-}
-
-protocol MainBaseCoordinator: Coordinator {
+protocol MainBaseCoordinator: CoordinatorType {
 //    var homeCoordinator: HomeBaseCoordinator { get }
 //    var ordersCoordinator: OrdersBaseCoordinator { get }
-//    var deepLinkCoordinator: DeepLinkBaseCoordinator { get }
     func moveTo(flow: AppFlow)
-    func handleDeepLink(text: String)
 }
 
 final class MainCoordinator {
