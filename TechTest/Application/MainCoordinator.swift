5c1cfcc9becb3e7b3d819f39c25e692fc72fc132
wyn <k022298@gmail.com>
2022-11-29 21:35:44 +0000

    Define HomeCoordinatorType, OrdersCoordinatorType, HomeCoordinator, OrdersCoordinator

--- TechTest/Application/MainCoordinator.swift
+++ TechTest/Application/MainCoordinator.swift
@@ -8,16 +8,79 @@
 import SwiftUI
 
 enum AppFlow {
-    case MostViewed
-    case Favorites
+    case postList
+    case savePostList
 }
 
-protocol MainBaseCoordinator: CoordinatorType {
-//    var homeCoordinator: HomeBaseCoordinator { get }
-//    var ordersCoordinator: OrdersBaseCoordinator { get }
+protocol HomeCoordinatorType: CoordinatorType {
+}
+
+protocol OrdersCoordinatorType: CoordinatorType {
+}
+
+class HomeCoordinator: HomeCoordinatorType {
+    lazy var rootViewController: UIViewController = UIViewController()
+    
+    func start() -> UIViewController {
+        return UIViewController()
+    }
+    
+    var parentCoordinator: MainCoordinatorType?
+    
+    
+}
+
+class OrdersCoordinator: OrdersCoordinatorType {
+    lazy var rootViewController: UIViewController = UIViewController()
+    
+    func start() -> UIViewController {
+        return UIViewController()
+    }
+    
+    var parentCoordinator: MainCoordinatorType?
+    
+    
+}
+
+
+protocol MainCoordinatorType: CoordinatorType {
+    var homeCoordinator: HomeCoordinatorType { get }
+    var ordersCoordinator: OrdersCoordinatorType { get }
     func moveTo(flow: AppFlow)
 }
 
-final class MainCoordinator {
+final class MainCoordinator: MainCoordinatorType {
+    var parentCoordinator: MainCoordinatorType?
+    lazy var rootViewController: UIViewController = UITabBarController()
+    lazy var homeCoordinator: HomeCoordinatorType = HomeCoordinator()
+    lazy var ordersCoordinator: OrdersCoordinatorType = OrdersCoordinator()
     
+    func start() -> UIViewController {
+        
+        let homeViewController = homeCoordinator.start()
+        homeCoordinator.parentCoordinator = self
+        homeViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "homekit"), tag: 0)
+        
+        let ordersViewController = ordersCoordinator.start()
+        ordersCoordinator.parentCoordinator = self
+        ordersViewController.tabBarItem = UITabBarItem(title: "Orders", image: UIImage(systemName: "doc.plaintext"), tag: 1)
+        
+        (rootViewController as? UITabBarController)?.viewControllers = [homeViewController,ordersViewController]
+        
+        return rootViewController
+    }
+    func moveTo(flow: AppFlow) {
+        switch flow {
+        case .postList:
+            (rootViewController as? UITabBarController)?.selectedIndex = 0
+        case .savePostList:
+            (rootViewController as? UITabBarController)?.selectedIndex = 1
+        }
+    }
+
+    func resetToRoot() -> Self {
+        homeCoordinator.resetToRoot()
+        moveTo(flow: .postList)
+        return self
+    }
 }
---
+++ TechTest/Support/Coordinator/CoordinatorType.swift
@@ -0,0 +1,31 @@
+//
+//  CoordinatorType.swift
+//  TechTest
+//
+//  Created by wyn on 2022/11/29.
+//
+
+import SwiftUI
+
+protocol FlowCoordinatorType: AnyObject {
+    var parentCoordinator: MainCoordinatorType? { get set }
+}
+
+protocol CoordinatorType: FlowCoordinatorType {
+    var rootViewController: UIViewController { get set }
+    func start() -> UIViewController
+    @discardableResult func resetToRoot() -> Self
+}
+
+extension CoordinatorType {
+    var navigationRootViewController: UINavigationController? {
+        get {
+            (rootViewController as? UINavigationController)
+        }
+    }
+    
+    func resetToRoot() -> Self {
+        navigationRootViewController?.popToRootViewController(animated: false)
+        return self
+    }
+}
