d820a56389a96b91f08aab03110b3618e70f428f
wyn <k022298@gmail.com>
2022-12-07 16:51:20 +0000

    Add testOpenPostDetails_tapSaveButtonOnce_PresentPostInSavePosts to TechTestUITests

--- TechTestUITests/TechTestUITests.swift
+++ TechTestUITests/TechTestUITests.swift
@@ -23,6 +23,31 @@ final class TechTestUITests: XCTestCase {
         saveButton.tap()
         XCTAssertTrue(savedButton.waitForExistence(timeout: 1))
         XCTAssertFalse(saveButton.waitForExistence(timeout: 1))
+        // recover to the unsaved status
+        savedButton.tap()
+        XCTAssertTrue(saveButton.waitForExistence(timeout: 1))
+        XCTAssertFalse(savedButton.waitForExistence(timeout: 1))
+    }
+
+    func testOpenPostDetails_tapSaveButtonOnce_PresentPostInSavePosts() {
+        let app = XCUIApplication()
+        let cell = app.collectionViews.cells.staticTexts["qui est esse"]
+        cell.tap()
+        let title = app.staticTexts["qui est esse"]
+        XCTAssertTrue(title.waitForExistence(timeout: 1))
+        let saveButton = app.buttons["Save"]
+        let savedButton = app.buttons["Saved"]
+        saveButton.tap()
+        XCTAssertTrue(savedButton.waitForExistence(timeout: 1))
+        XCTAssertFalse(saveButton.waitForExistence(timeout: 1))
+        let tabBarBookmark = app.tabBars["Tab Bar"].buttons["BOOKMARK"]
+        let tabBarPost = app.tabBars["Tab Bar"].buttons["POST"]
+        tabBarBookmark.tap()
+        XCTAssertTrue(cell.waitForExistence(timeout: 1))
+        tabBarPost.tap()
+        XCTAssertTrue(savedButton.waitForExistence(timeout: 1))
+        XCTAssertFalse(saveButton.waitForExistence(timeout: 1))
+        // recover to the unsaved status
         savedButton.tap()
         XCTAssertTrue(saveButton.waitForExistence(timeout: 1))
         XCTAssertFalse(savedButton.waitForExistence(timeout: 1))
